eaa4795a450d102e582b4fbfd06e94c8740dfce2
EliDimi <2275eli@gmail.com>
2024-11-12 18:29:36 +0200

    Create task_1.cpp

---
+++ Sem.01/Pract.01/task_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main1() {
+    int a, b;
+    cout << "Dividend: ";
+    cin >> a;
+    cout << "Divisor: ";
+    cin >> b;
+    cout << "The quotient of the devision: " << (a / b) << endl;
+    cout << "The remainder of the devision: " << (a % b) << endl;
+
+    return 0;
+}
+
+
+
+
+
+
